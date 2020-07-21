396901fd53627596aa28b2b24b71565df802dc08
bloomlj <kollocc@live.com>
2020-07-21 10:00:06 +0800

    Update Configuration.h

--- 01_corexy_3dprintr/Configuration.h
+++ 01_corexy_3dprintr/Configuration.h
@@ -762,7 +762,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 110 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 90 }
 
 /**
  * Default Max Feed Rate (mm/s)
